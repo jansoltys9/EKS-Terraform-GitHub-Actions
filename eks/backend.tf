053c5cd93009b0f80b7f24e999feda6c4f0a2514
jansoltys9 <jansoltys9@proton.me>
2025-01-14 12:35:46 +0100

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-aws-baket-abcd"
+    bucket         = "my-ews-bucket12345"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "Lock-Files-jenkins"
     encrypt        = true
   }
 }
